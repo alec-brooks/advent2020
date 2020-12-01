448556b7c5dff465d40be98f6e5e9696612f90b8
Alec Brooks <abrooks@squareup.com>
2020-12-01 16:34:59 +1100

    Better looking day 1

--- 1.py
+++ 1.py
@@ -1,13 +1,18 @@
 import sys
+from itertools import combinations
+from math import prod
 
 
 def main():
-    nums = [int(x) for x in sys.stdin]
-    for ni in nums:
-        for nj in nums:
-            for nk in nums:
-                if ni + nj + nk == 2020:
-                    print(ni * nj * nk)
+    report = [int(x) for x in sys.stdin]
+    print(expense_summing(report, 2020, 2))
+    print(expense_summing(report, 2020, 3))
+
+
+def expense_summing(report, target, number_of_items):
+    for combo in combinations(report, number_of_items):
+        if sum(combo) == target:
+            return prod(combo)
 
 
 main()
