7c72b6e3a3525b43b8b393e1774a12cec797a4b9
Alec Brooks <abrooks@squareup.com>
2020-12-01 16:34:35 +1100

    Hacky day 1

--- 1.py
+++ 1.py
@@ -1,6 +1,13 @@
 import sys
 
+
 def main():
-    pass
+    nums = [int(x) for x in sys.stdin]
+    for ni in nums:
+        for nj in nums:
+            for nk in nums:
+                if ni + nj + nk == 2020:
+                    print(ni * nj * nk)
+
 
 main()
