e49300e0ce5cda7d796d9213d5d8b36fdbebd3af
Alec Brooks <alec.e.brooks@gmail.com>
2020-12-02 20:50:33 +1100

    Nicer day 2?

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def parseLine(l):
+def parse_line(l):
     inputs = l.split(" ")
     positions = [int(x) for x in inputs[0].split('-')]
     target = inputs[1].strip(':')
@@ -10,13 +10,26 @@ def parseLine(l):
     return positions, target, passwd
 
 
+def count_falls_in_range(string, char, count_range):
+    return string.count(char) in count_range
+
+
+def one_index_has_char(string, char, indices):
+    return (string[indices[0]-1] == char) != (string[indices[1]-1] == char)
+
+
 def main():
-    c = 0
-    for line in sys.stdin:
-        positions, char, pwd = parseLine(line)
-        if (pwd[positions[0]-1] == char) != (pwd[positions[1]-1] == char):
-            c += 1
-    print(c)
+    parsed_lines = [parse_line(l) for l in sys.stdin]
+
+    ans1 = sum([
+        count_falls_in_range(password, target, range(ns[0], ns[1]+1))
+        for ns, target, password in parsed_lines
+    ])
+    ans2 = sum([
+        one_index_has_char(password, target, ns)
+        for ns, target, password in parsed_lines
+    ])
+    print(ans1, ans2)
 
 
 main()
