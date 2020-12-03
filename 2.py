0e112164ae4794dd9594516e56f3bfa8663da96a
Alec Brooks <abrooks@squareup.com>
2020-12-03 12:26:58 +1100

    Parse day 2 with regex, linting

--- 2.py
+++ 2.py
@@ -1,12 +1,10 @@
 import sys
+import re
 
 
 def parse_line(l):
-    inputs = l.split(" ")
-    positions = [int(x) for x in inputs[0].split('-')]
-    target = inputs[1].strip(':')
-    passwd = inputs[2]
-
+    pos, target, passwd = re.match(r"(\d+-\d+) (.): (.*)", l).groups()
+    positions = [int(x) for x in pos.split("-")]
     return positions, target, passwd
 
 
@@ -15,20 +13,24 @@ def count_falls_in_range(string, char, count_range):
 
 
 def one_index_has_char(string, char, indices):
-    return (string[indices[0]-1] == char) != (string[indices[1]-1] == char)
+    return (string[indices[0] - 1] == char) != (string[indices[1] - 1] == char)
 
 
 def main():
     parsed_lines = [parse_line(l) for l in sys.stdin]
 
-    ans1 = sum([
-        count_falls_in_range(password, target, range(ns[0], ns[1]+1))
-        for ns, target, password in parsed_lines
-    ])
-    ans2 = sum([
-        one_index_has_char(password, target, ns)
-        for ns, target, password in parsed_lines
-    ])
+    ans1 = sum(
+        [
+            count_falls_in_range(password, target, range(ns[0], ns[1] + 1))
+            for ns, target, password in parsed_lines
+        ]
+    )
+    ans2 = sum(
+        [
+            one_index_has_char(password, target, ns)
+            for ns, target, password in parsed_lines
+        ]
+    )
     print(ans1, ans2)
 
 
